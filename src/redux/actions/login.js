314c230bf690508b0d453fd4d36b8fbe45b2f10e
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2020-01-16 15:10:11 -0300

    Add redux login logic

--- src/redux/actions/login.js
+++ src/redux/actions/login.js
@@ -1,11 +1,36 @@
-import * as types from '../types';
+import types from '../types';
 import * as UserService from '../../services/api/user';
 
 export const do_login = (user, password) => {
   return async dispatch => {
-    console.log('=====================');
-    console.log('user', user);
-    console.log('password', password);
-    console.log('=====================');
+
+    dispatch(loginStart());
+
+    const loginService = await UserService.login(user, password);
+
+    if( loginService !== null ){
+      dispatch(loginSucceed());
+    } else {
+      dispatch(loginFailed());
+    }
+
+  };
+};
+
+const loginStart = () => {
+  return {
+    type: types.LOGIN,
+  };
+};
+
+const loginSucceed = () => {
+  return {
+    type: types.LOGIN_SUCCEEDED,
+  };
+};
+
+const loginFailed = () => {
+  return {
+    type: types.LOGIN_FA,
   };
 };
--- src/redux/types/login.js
+++ src/redux/types/login.js
@@ -2,3 +2,4 @@ export const LOGIN = 'LOGIN';
 export const LOGIN_SUCCEEDED = 'LOGIN_SUCCEEDED';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 export const LOGOUT = 'LOGOUT';
+export const LOGIN_LOADING = 'LOGIN_LOADING';
