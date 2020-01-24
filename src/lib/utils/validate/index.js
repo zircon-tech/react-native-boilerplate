e694773360df2468d3d6e2806549c24eb58c19ef
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2020-01-24 12:01:46 -0300

    Remove unused var

--- src/lib/utils/validate/index.js
+++ src/lib/utils/validate/index.js
@@ -2,11 +2,9 @@ import validatejs from 'validate.js';
 import validations from './validations';
 
 export default validate = fields => {
-  const formValues = [];
   const formFields = [];
 
   for (var key in fields) {
-    formValues[key] = fields[key];
     formFields[key] = validations[key];
   }
 
